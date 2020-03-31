adab6fd7d0d63bee7b8672efbace609c30d584b5
Gauvin Alexandre <alexandre.gauvin@iut-rodez.fr>
2020-03-31 11:22:37 +0200

    fix #2.5.2 generated SQL

--- src/main/java/ourbusinessproject/EnterpriseProjectService.java
+++ src/main/java/ourbusinessproject/EnterpriseProjectService.java
@@ -41,6 +41,13 @@ public class EnterpriseProjectService {
     }
 
     public List<Project> findAllProjects() {
+        /*
+           1 : 2 requêtes identiques sont générées (seul le paramètre change)
+           2 : Elles retournent les informations des entreprises
+           3 : Ce sont les informations des entreprises liés au projet
+           4 : Ces informations ne sont pas retournés dans la première requête
+               car il n'y a pas de jointure juste une référence (clé étrangère) aux entreprises dans celle-ci
+         */
         String query = "SELECT p FROM Project p ORDER BY p.title" ;
         TypedQuery<Project> queryObj = entityManager.createQuery(query,Project.class);
         return queryObj.getResultList();
